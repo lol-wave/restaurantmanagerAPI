0242037956a97b694dbe215b6b2f39582d178fb5
lol-wave <unk.aminjon@gmail.com>
2026-09-18 17:09:30 +0500

    restaurant model change

--- src/modules/restaurants/models.py
+++ src/modules/restaurants/models.py
@@ -7,14 +7,24 @@ class RestaurantModel(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True, nullable=False)
-    address = Column(String, nullable=False)
-    contact_number = Column(String, nullable=False)
-    contact_email = Column(String, nullable=False)
-    instagram_link = Column(String, nullable=True)
-    website_link = Column(String, nullable=True)
-    wifi_ssid = Column(String, nullable=True)
+    description = Column(String, nullable=True)
+    longitude = Column(String, nullable=True)
+    latitude = Column(String, nullable=True)
+    wifi_name = Column(String, nullable=True)
     wifi_password = Column(String, nullable=True)
-    working_hours = Column(JSON, nullable=False, default=dict)
-
-    owner_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+    phone_number = Column(String, nullable=True)
+    email = Column(String, nullable=False)
+    contacts = relationship("ContactModel", back_populates="restaurant")
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     owner = relationship("UserModel", back_populates="restaurants")
+
+
+class ContactModel(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    restaurant_id = Column(Integer, ForeignKey("restaurants.id"), nullable=False)
+    instagram = Column(String, nullable=True)
+    facebook = Column(String, nullable=True)
+    telegram = Column(String, nullable=True)
+    restaurant = relationship("RestaurantModel", back_populates="contacts")
